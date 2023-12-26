1f19f368090a201309ab8e2c7ea3280aea934133
José Neilson <66130311+jose-neilson@users.noreply.github.com>
2023-12-26 18:56:32 +0000

    adicionar pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Eae meu parceiro, já tomou um copo da água? Ajeita tua coluna e depois manda foto do copo d'água se viu a mensagem kkkk. <br/>
+    </h1>
+}
+
+export default Home;
