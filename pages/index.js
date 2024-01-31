de02cb0c481b01a3326902b3358a831919bc006b
José Neilson <66130311+jose-neilson@users.noreply.github.com>
2024-01-31 14:08:43 +0000

    adiciona scripts `link:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
-    return <h1>Eae meu amigo, venho aqui desejar muita felicidade pra ti. <br/>
-    Espero que esse ano de 2024 possa ser um anos bom para todos nós, mas inclusive para você meu irmão ^^.
+  return (
+    <h1>
+      Eae meu amigo, venho aqui desejar muita felicidade pra ti. <br />
+      Espero que esse ano de 2024 possa ser um anos bom para todos nós, mas
+      inclusive para você meu irmão ^^.
     </h1>
+  );
 }
 
 export default Home;
